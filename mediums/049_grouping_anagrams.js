cfec598e94e2b8c1aa1b6718093f515bec236c80
KevinTrinh1227 <kevintrinh1227@gmail.com>
2024-03-15 19:08:34 -0500

    updated grouping anagrams to use a , to seperate items in arr

--- mediums/049_grouping_anagrams.js
+++ mediums/049_grouping_anagrams.js
@@ -10,10 +10,10 @@ var groupAnagrams = function (strs) {
 
     for (j = 0; j < strs[i].length; j++) {
       let charIDX = strs[i][j].charCodeAt() - "a".charCodeAt();
-      wordID[charIDX] += 1 + wordID[charIDX];
+      wordID[charIDX]++;
     }
 
-    wordID = wordID.join("");
+    wordID = wordID.join(",");
     if (words.has(wordID)) {
       let currWords = words.get(wordID);
       currWords.push(strs[i]);
